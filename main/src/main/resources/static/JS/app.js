a00cd460fd73cb23f10d1a0914e1690c50847b0b
CoalReef <colerife80@gmail.com>
2026-07-08 21:11:19 -0400

    Some frontend work
    Made the card to add manga be hidden on page open and open when user clicks the add button.
    Did a few other CSS touch ups, made it look a little prettier

--- main/src/main/resources/static/JS/app.js
+++ main/src/main/resources/static/JS/app.js
@@ -1,7 +1,17 @@
 // Pre config of variables for document elements
-const inputBox = document.getElementById("text-box");
+// Input fields
+const inputBox = document.getElementById("add-search-bar");
+
+// Buttons
 const searchButton = document.getElementById("search-button");
-const outputTest = document.getElementById("data-return");
+const addMangaButton = document.getElementById("add-manga-button");
+
+// Output Fields
+const outputTest = document.getElementById("search-results");
+
+// Div/Wrappers
+const addCardWrapper = document.getElementById("add-manga-card");
+const addCardBackground = document.getElementById("card-background");
 
 // Call for the data asynchronously
 async function getMangaInfo(mangaName) {
@@ -9,13 +19,26 @@ async function getMangaInfo(mangaName) {
     return await response.json();
 }
 
-
+// Fetch API Data based on user input on click of confim search button
 searchButton.addEventListener("click", async function startMangaSearch()  {
     const mangaInfo = await getMangaInfo(inputBox.value);
-
     outputTest.innerHTML = mangaInfo.title;
     console.log(mangaInfo);
 });
+
+// Open pop up window for adding manga
+addMangaButton.addEventListener("click", function openAddWindow() {
+    inputBox.style.setProperty("display", "block");
+    searchButton.style.setProperty("display", "block");
+    outputTest.style.setProperty("display", "block");
+    addCardBackground.style.setProperty("display", "flex");
+    addCardWrapper.style.setProperty("display", "grid");
+});
+
+// Close the pop up window
+
+
+
 
 
 
