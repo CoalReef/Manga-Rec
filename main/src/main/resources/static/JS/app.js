40c5c21d4584a247e93f294df6ca82d2e3540a80
CoalReef <colerife80@gmail.com>
2026-07-09 22:05:03 -0400

    Close button
    Did a couple of CSS Style updates and added a button to close the pop up window

--- main/src/main/resources/static/JS/app.js
+++ main/src/main/resources/static/JS/app.js
@@ -5,6 +5,7 @@ const inputBox = document.getElementById("add-search-bar");
 // Buttons
 const searchButton = document.getElementById("search-button");
 const addMangaButton = document.getElementById("add-manga-button");
+const closeCardButton = document.getElementById("close-card-button");
 
 // Output Fields
 const outputTest = document.getElementById("search-results");
@@ -31,11 +32,19 @@ addMangaButton.addEventListener("click", function openAddWindow() {
     inputBox.style.setProperty("display", "block");
     searchButton.style.setProperty("display", "block");
     outputTest.style.setProperty("display", "block");
+    closeCardButton.style.setProperty("display", "block");
     addCardBackground.style.setProperty("display", "flex");
     addCardWrapper.style.setProperty("display", "grid");
 });
 
-// Close the pop up window
+closeCardButton.addEventListener("click", function closeCardButton() {
+    inputBox.style.setProperty("display", "none");
+    searchButton.style.setProperty("display", "none");
+    outputTest.style.setProperty("display", "none");
+    addCardBackground.style.setProperty("display", "none");
+    addCardWrapper.style.setProperty("display", "none");
+    closeCardButton.style.setProperty("display", "none");
+});
 
 
 
